f6d967b1cae4c68a9824b65fb4402ec58fc17d21
graba0maroua <116178092+graba0maroua@users.noreply.github.com>
2024-05-04 22:26:34 +0100

    add comment and clean code

--- src/etl.py
+++ src/etl.py
@@ -1,7 +1,21 @@
 import os
 from utils import *
 import pandas as pd
+#! uncomment what u want to run
+# TODO : make sure to change the folder_path in utils.js to your own after adding the Moroco and tunisia folders in preprocessed folder  
 # drop_TAVG_TAVG_ATTRIBUTES()
+
 # fill_missing_TMIN()
+# fill_missing_TMIN()
+
 # fill_missing_TMAX()
-fill_missing_PRCP()
+# fill_missing_TMAX_prev()
+
+# fill_missing_PRCP()
+# fill_missing_PRCP_prev()
+
+
+
+
+
+
