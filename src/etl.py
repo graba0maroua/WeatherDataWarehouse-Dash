d964f9b8f3bbd7e49870530da5261f507d5502ed
graba0maroua <116178092+graba0maroua@users.noreply.github.com>
2024-05-04 19:18:01 +0100

    fill missing TMIN values

--- src/etl.py
+++ src/etl.py
@@ -1,9 +1,7 @@
 import os
-from utils import drop_columns
+from utils import *
+import pandas as pd
 
-# Define the folder path
-folder_path = os.path.join('data', 'raw', 'Algeria')
-# Define the columns to drop
-columns_to_drop = ['TAVG', 'TAVG_ATTRIBUTES']
-# Call the function from utils.py to drop the specified columns from all CSV files in the folder
-drop_columns(folder_path, columns_to_drop)
+# drop_TAVG_TAVG_ATTRIBUTES()
+# fill_missing_TMIN()
+
