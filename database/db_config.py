46e0175060be19f50d4e50c466a4ef9fafa4a60a
graba0maroua <116178092+graba0maroua@users.noreply.github.com>
2024-05-05 15:51:40 +0100

    config edits

--- database/db_config.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def create_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Marouabestfullstackdev231220*",
-        database="weather_datawarehouse"
-    )
