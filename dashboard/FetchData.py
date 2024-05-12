e7554670678d66a353c8ce5af1152c141ba75bf0
graba0maroua <116178092+graba0maroua@users.noreply.github.com>
2024-05-12 19:47:46 +0100

    update create chart

--- dashboard/FetchData.py
+++ dashboard/FetchData.py
@@ -1,9 +1,9 @@
-import mysql.connector
 from db_config import create_connection
 
 # Récupérer les données climatiques depuis la base de données
 connection = create_connection()
 
+# Fetch precipitation data for a specific country
 def fetch_precipitation_data(country):
     query = f"""
         SELECT t.Année , t.Mois, mm.precipitation
@@ -17,3 +17,30 @@ def fetch_precipitation_data(country):
     data = cursor.fetchall()
     cursor.close()
     return data
+
+def fetch_precipitation_data_at_station(city, selected_country):
+    query = f"""
+        SELECT t.Année AS Year, t.Mois, mm.precipitation
+        FROM mesures_météorologiques mm
+        INNER JOIN temps t ON mm.id_date = t.id_date
+        INNER JOIN station s ON mm.id_station = s.id_station
+        WHERE s.Ville = '{city}' 
+            AND s.Pays = '{selected_country}'
+    """
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(query)
+    data = cursor.fetchall()
+    cursor.close()
+    return data
+
+# Fetch stations data
+def fetch_stations():
+    query = """
+        SELECT DISTINCT ville
+        FROM station
+    """
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute(query)
+    stations = cursor.fetchall()
+    cursor.close()
+    return stations
--- dashboard/app.py
+++ dashboard/app.py
@@ -3,12 +3,24 @@ import pandas as pd
 from dash import dcc, html
 from dash.dependencies import Input, Output
 import plotly.express as px
-from FetchData import fetch_precipitation_data
+from FetchData import fetch_precipitation_data_at_station, fetch_stations
 
 app = dash.Dash(__name__)
 
+# Fetch station data for dropdown
+stations = fetch_stations()  
+station_options = [{'label': station['ville'], 'value': station['ville']} for station in stations]
+
+# Define the layout of the app
 app.layout = html.Div([
-    html.H1("Evolution of Precipitation by Country"),
+    html.H1("Evolution of Precipitation by Station"),
+    dcc.Dropdown(
+        id='station-dropdown',
+        options=station_options,
+        value=stations[0]['ville'],  # Default value to the first station
+        clearable=False,
+        placeholder="Select a City"
+    ),
     dcc.Dropdown(
         id='country-dropdown',
         options=[
@@ -17,25 +29,31 @@ app.layout = html.Div([
             {'label': 'Tunisia', 'value': 'TS'}
         ],
         value='AG',
-        clearable=False
+        clearable=False,
+        placeholder="Select a Country"
     ),
     dcc.Graph(id='precipitation-graph')
 ])
 
+# Define the callback to update the graph based on user inputs
 @app.callback(
     Output('precipitation-graph', 'figure'),
-    [Input('country-dropdown', 'value')]
+    [Input('station-dropdown', 'value'),
+    Input('country-dropdown', 'value')]
 )
-def update_precipitation_graph(country):
+def update_precipitation_graph(city,selected_country):
     try:
-        data = fetch_precipitation_data(country)
+        data = fetch_precipitation_data_at_station(city,selected_country)
         df = pd.DataFrame(data)
-        fig = px.line(df, x='Année', y='precipitation',
-                title=f'Evolution of Precipitation in {country}',
-                labels={'precipitation': 'Precipitation ', 'Année': 'Année'})
+        
+        # Create the line chart
+        fig = px.line(df, x='Year', y='precipitation',color='Mois', markers=True, 
+                      title=f'Evolution of Precipitation Over Years at {city} ( {selected_country})')
+        fig.update_xaxes(title_text='Year')
+        fig.update_yaxes(title_text='Precipitation (mm)')
         return fig
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"An error occurred: {str(e)}")
         return {}
 
 if __name__ == '__main__':
