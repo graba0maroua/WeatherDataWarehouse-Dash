76f7409b570804e2ad4dc11df14f9ce496e188e4
graba0maroua <116178092+graba0maroua@users.noreply.github.com>
2024-05-21 10:43:50 +0100

    merge 'mapbox v4' into main

--- dashboard/mapbox.py
+++ dashboard/mapbox.py
@@ -10,14 +10,13 @@ app = dash.Dash(__name__)
 def establish_database_connection():
     try:
         # conn = mysql.connector.connect(
-        #     host="......",
+        #     host=".....",
         #     database="..........",
-        #     user=".........",
+        #     user="........",
         #     password="........"
         # )
-        print(f"database connected")
+        print("database connected")
         return conn
-        
     except mysql.connector.Error as e:
         print(f"Error connecting to MySQL database: {e}")
         return None
@@ -26,26 +25,18 @@ def establish_database_connection():
 conn = establish_database_connection()
 
 def update_weather_map(selected_countries, start_date, end_date):
-    global conn  # Declare conn as global
-
+    global conn
     if not selected_countries or start_date is None or end_date is None:
-        return pd.DataFrame()  # Return an empty DataFrame
-
-    # Re-establish MySQL connection if it's closed or lost
+        return pd.DataFrame()  
     if conn is None or not conn.is_connected():
         conn = establish_database_connection()
-
     if conn is None:
         print("MySQL connection is not available.")
-        return pd.DataFrame()  # Return an empty DataFrame
+        return pd.DataFrame() 
 
-    # Construct the IN clause for multiple countries
     country_in_clause = ", ".join(["'%s'" % country for country in selected_countries])
-
-    # Execute SQL query to fetch data from the database for multiple countries
     cursor = conn.cursor(dictionary=True)
     cursor.execute(f"""
-    
     SELECT station.Ville, station.Latitude, station.Longitude,
         MAX(Mesures_Météorologiques.temperature_max) AS temperature_max,
         MIN(Mesures_Météorologiques.temperature_min) AS temperature_min,
@@ -55,34 +46,33 @@ def update_weather_map(selected_countries, start_date, end_date):
     JOIN temps ON Mesures_Météorologiques.id_date = temps.id_date
     WHERE station.Pays IN ({country_in_clause}) AND temps.Date BETWEEN %s AND %s
     GROUP BY station.Ville, station.Latitude, station.Longitude
-
-
-""", (start_date, end_date))
-
+    """, (start_date, end_date))
     rows = cursor.fetchall()
+    df = pd.DataFrame(rows)
+    if not df.empty:
+        df['precipitation'] = df['precipitation'].round(3)
+    return df
 
-    return pd.DataFrame(rows)  # Convert the fetched data into a DataFrame
 
-# Define CSS styles for elements
 external_stylesheets = {
-    'background-color': '#f8f9fa',
+    'background-color': '#ffffff',
     'padding': '20px'
 }
 
-# Apply styles to components
+
 app.layout = html.Div(style=external_stylesheets, children=[
-    html.H1('Weather Data Visualization', style={'text-align': 'center'}),
+    html.H1('Visualisation des données météorologiques', style={'text-align': 'center'}),
     html.Div([
         dcc.Dropdown(
             id='country-dropdown',
             options=[
-                {'label': 'Algeria', 'value': 'AG'},
-                {'label': 'Morocco', 'value': 'MO'},
-                {'label': 'Tunisia', 'value': 'TS'},  
+                {'label': 'Algerie', 'value': 'AG'},
+                {'label': 'Maroc', 'value': 'MO'},
+                {'label': 'Tunisie', 'value': 'TS'},
             ],
-            value=['MO'],  # Set default value to MO
-            multi=True,  # Allow multiple selections
-            style={'width': '50%'}
+            value=['MO'],  
+            multi=True,  
+            style={'width': '45%', 'margin-right': '10px'}
         ),
         dcc.DatePickerRange(
             id='date-range',
@@ -91,20 +81,19 @@ app.layout = html.Div(style=external_stylesheets, children=[
             initial_visible_month=pd.to_datetime('2020-01-01'),
             start_date=pd.to_datetime('2020-01-01').strftime('%Y-%m-%d'),
             end_date=pd.to_datetime('2020-01-10').strftime('%Y-%m-%d'),
-            style={'margin-top': '10px'}
+            style={'width': '45%', 'margin-left': '10px'}
         ),
-    ], style={'display': 'flex', 'justify-content': 'center'}),
+    ], style={'display': 'flex', 'justify-content': 'center', 'align-items': 'center', 'margin-bottom': '20px'}),
     dcc.Loading(
         id="loading-weather-map",
         type="default",
         children=[
             dcc.Graph(id='weather-map')
-        ]
+        ],
+        style={'margin-bottom': '20px'}
     ),
     html.Div(id='weather-data', style={'margin-top': '20px', 'text-align': 'center'})
 ])
-
-# Define callback to update the map
 
 @app.callback(
     Output('weather-map', 'figure'),
@@ -113,23 +102,20 @@ app.layout = html.Div(style=external_stylesheets, children=[
      Input('date-range', 'end_date')]
 )
 def update_map(selected_countries, start_date, end_date):
-    # Fetch weather data from the database
+    
     print(f"Selected Countries: {selected_countries}")
     print(f"Start Date: {start_date}, End Date: {end_date}")
     weather_data = update_weather_map(selected_countries, start_date, end_date)
 
-    # Check if weather data is empty
     if weather_data.empty:
         return {}
 
-    # Create a scatter map plot using Plotly Express
     fig = px.scatter_mapbox(weather_data, lat="Latitude", lon="Longitude",
                             hover_name="Ville",
                             hover_data={"precipitation": True,
                                         "temperature_min": True,
                                         "temperature_max": True},
-                            zoom=3, height=600,
-                           )
+                            zoom=3, height=600)
 
     fig.update_layout(title=f"Weather in {' & '.join(selected_countries)} ({start_date} - {end_date})",
                       mapbox_style="open-street-map",
